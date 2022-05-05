e482e7cd13746c7494c4dde7d92ca60d156bf356
theJoseNeto <joseneto180201@gmail.com>
2022-05-05 18:47:26 -0300

    readme adicionado

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ require('dotenv').config() ? console.log("In development...") : console.log('pro
 const {Client,Intents} = require('discord.js');
 const app = require('./src/app');
 const client = new Client({intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS]});
+
 const tokenAccess = process.env.DISCORD_TOKEN_ACCESS;
-const channelIdYoutube = process.env.TEST_CHANNEL;
+const channelIdYoutube = process.env.VIDEO_PROMOTION_CHANNEL;
 
 setInterval(() => {
-    client.login(tokenAccess)
-        .then(() => {
-            app(client, channelIdYoutube);
-        });
+    
+    client.login(tokenAccess).then(() => app(client, channelIdYoutube));
 
 }, 3600000); // 3600000 = 1 hour in miliseconds
